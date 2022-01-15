3f6d05d4916e6c594f844b6fb02277737f38df51
Stefan Feltenmark <stefan.feltenmark@volue.com>
2022-01-15 17:29:20 +0100

    Add project files.

---
+++ DimensionAndSort/Exceptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DimensionAndSort
+{
+    public class Exceptions
+    {
+
+    }
+
+    public class IncompatibleUnits : Exception
+    {
+        public IncompatibleUnits(string message) : base(message)
+        {
+        }
+
+        public IncompatibleUnits() : base()
+        {
+        }
+    }
+
+}
